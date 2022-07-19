9d2f01d249ef09e494313116a03c96f0d6974e51
Royce Strange <roycest@swbell.net>
2022-07-18 22:32:07 -0500

    generate an html template and modularize

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js')
+
 const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
 
-printProfileData(profileDataArgs);
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw err;
+    console.log("Portfolio complte! Check out index.html to see the output");
+});
